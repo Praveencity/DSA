76e15e2931c4b317709c8047e079e7383868d8b6
Praveen Bhandari <praveenbhandari0987@gmail.com>
2025-12-20 00:45:22 +0530

    Update FrogJump solution

--- 12. Dynamic Programming/1D_DP/3.FrogJump.cpp
+++ 12. Dynamic Programming/1D_DP/3.FrogJump.cpp
@@ -6,19 +6,34 @@ https://takeuforward.org/plus/dsa/problems/frog-jump-with-k-distances
 #include <bits/stdc++.h>
 using namespace std;
 
-int rec(vector<int> &v,int i,int k){
+// ? Done With Recursion and Memoization.
+int rec(vector<int> &v,int i,int k,vector<int> &dp){
     if(i==0) return 0;
-    
+    if(dp[i] != -1)
+        return dp[i];
     int mini = INT_MAX;
     for(int j=1;j<=k;j++){
         if(i>=j)
-            mini = min(mini,rec(v,i-j,k) + abs(v[i]-v[i-j]));
+            mini = min(mini,rec(v,i-j,k,dp) + abs(v[i]-v[i-j]));
     }
-    return mini;
+    return dp[i] = mini;
 }
 
 int frogJump(vector<int>& heights, int k) {
-    return rec(heights,heights.size()-1,k);
+    vector<int> dp(heights.size(),-1);
+    //return rec(heights,heights.size()-1,k,dp);
+
+    // ? Tabulation(Iterative).
+    dp[0] = 0;
+    for(int i=1;i<heights.size();i++){
+        int mini = INT_MAX;
+        for(int j=1;j<=k;j++){
+            if(i>=j)
+                mini = min(mini,dp[i-j] + abs(heights[i]-heights[i-j]));
+        }
+        dp[i] = mini;
+    }
+    return dp[heights.size()-1];
 }
 
 int main(){
